5dfa36f6cd3bcadec7f17a3c86dbb9d385c32c5e
Alan <alan-hd@hotmail.com>
2024-05-06 10:12:50 -0300

    Revert "Implemtando mais componentes"
    This reverts commit 0c06803cf4a38ec17c361d0470d2ae24078e8567.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,8 +5,8 @@ import homeBanner from '../../assets/img/banner-naturalar.png'
 function Home() {
     return (
       <>
-        <div className="flex justify-center">
-        <img src={homeBanner} alt="Logo da Naturalar" className=' imagem' />
+        <div className="bg-indigo-900 flex justify-center teste">
+        <img src={homeBanner} alt="Logo da Naturalar" className='w-3/3 imagem' />
         </div>
       </>
     );
