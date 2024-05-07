a3ba2e20d76d2a720ec6007e71c1fbbe9800efd5
biel678 <bieltomarchio@hotmail.com>
2024-05-07 15:43:07 -0300

    finalizando a lógica de login e cadastro

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,8 +5,8 @@ import homeBanner from '../../assets/img/banner-naturalar.png'
 function Home() {
     return (
       <>
-        <div className=" ">
-          <h1>HOME </h1>
+        <div className="bg-indigo-900 flex justify-center teste">
+        <img src={homeBanner} alt="Logo da Naturalar" className='w-3/3 imagem' />
         </div>
       </>
     );
