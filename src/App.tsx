84707e34d167624b34b861edaea52b663fbeedab
Kamilla Lima <kamillalima868@gmail.com>
2024-05-07 12:09:29 -0300

    alterando a tela de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,27 @@ import Home from './paginas/home/Home'
 import Sobre from './paginas/sobre/Sobre'
 import Login from './paginas/login/Login'
 import Cadastro from './paginas/cadastro/Cadastro'
-function App(){
+import { AuthProvider } from './contexts/AuthContext';
+function App() {
   return (
-  <>
-    <BrowserRouter>
-      <Menu />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/sobre' element={<Sobre />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/cadastro' element={<Cadastro/>}/>
-          </Routes>
-        </div>
-      <Rodape />
-    </BrowserRouter>
-  </>
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Menu />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path='/sobre' element={<Sobre />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/cadastro' element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Rodape />
+        </BrowserRouter>
+      </AuthProvider>
+
+    </>
   )
 }
 
