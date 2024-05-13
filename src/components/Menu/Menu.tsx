cc5cd9884d38e309cff06149b856f6a3ef99774d
Raul-Prado <rauloliveiraprado@gmail.com>
2024-05-13 11:31:10 -0300

    Ajustes perfis

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,25 +1,80 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useInRouterContext } from 'react-router-dom'
 import './Menu.css'
 import homeLogo from '../../assets/img/logo-nome-naturalar.webp'
+import { AuthContext } from '../../contexts/AuthContext'
 
-function Menu(){
-  return (
-    <>
-    <div className='w-full navbar text-white flex justify-center py-4 bg-green-dark'>
+function Menu() {
+  let navbarComponent
+  const { usuario } = useContext(AuthContext)
+  let tipo: string = usuario.tipo
+
+  if (tipo === "cliente") {
+    navbarComponent = (
+      <div className='w-full navbar text-white flex justify-center py-4 bg-green-dark'>
         <div className="container flex justify-between text-lg">
           <div className='text-2xl font-bold uppercase'>
-              <img src={homeLogo} alt="Logo da Naturalar" className='w-2/3 imagem' />
+            <img src={homeLogo} alt="Logo da Naturalar" className='w-2/3 imagem' />
           </div>
 
           <div className='flex gap-4'>
             <Link to='/home' className='hover:underline'>Home</Link>
             <Link to='/sobre' className='hover:underline'>Sobre</Link>
             <Link to='/produtos' className='hover:underline'>Produtos</Link>
+            <Link to='/' className='hover:underline'>Logout</Link>
+          </div>
+        </div>
+      </div>
+    )
+  } 
+  
+  if (tipo === "funcionario") {
+    navbarComponent = (
+      <div className='w-full navbar text-white flex justify-center py-4 bg-green-dark'>
+        <div className="container flex justify-between text-lg">
+          <div className='text-2xl font-bold uppercase'>
+            <img src={homeLogo} alt="Logo da Naturalar" className='w-2/3 imagem' />
+          </div>
+
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Home</Link>
+            <Link to='/sobre' className='hover:underline'>Sobre</Link>
+            <Link to='/produtos' className='hover:underline'>Produtos</Link>
+            <Link to='/' className='hover:underline'>Categoria</Link>
+            <Link to='/' className='hover:underline'>Logout</Link>
+          </div>
+        </div>
+      </div>
+    )
+
+  } 
+  
+  if (tipo=== "") {
+    navbarComponent = (
+      <div className='w-full navbar text-white flex justify-center py-4 bg-green-dark'>
+        <div className="container flex justify-between text-lg">
+          <div className='text-2xl font-bold uppercase'>
+            <img src={homeLogo} alt="Logo da Naturalar" className='w-2/3 imagem' />
+          </div>
+
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Home</Link>
+            <Link to='/sobre' className='hover:underline'>Sobre</Link>
+            <Link to='/produtos' className='hover:underline'>Produtos</Link>
+            <Link to='/' className='hover:underline'>Categoria</Link>
             <Link to='/login' className='hover:underline'>Login</Link>
           </div>
         </div>
       </div>
+    )
+  }
+
+  console.log(tipo)
+  console.log(usuario)
+
+  return (
+    <>
+      {navbarComponent}
     </>
   )
 }
