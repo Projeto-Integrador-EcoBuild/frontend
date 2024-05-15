7d53cfd78038b54b9fdc05530f4d62abbfe88745
marii182 <maria.sivaxavier18@gmail.com>
2024-05-15 14:06:26 -0300

    arrumando categoria

--- src/components/Categoria/cardCategoria/cardCategoria.tsx
+++ src/components/Categoria/cardCategoria/cardCategoria.tsx
@@ -9,7 +9,7 @@ interface CardCategoriaProps {
 function CardCategoria({categoria}: CardCategoriaProps) {
   return (
     <div className='border flex flex-col rounded-1xl overflow-hidden justify-between border-gray-400'>
-      <header className="py-2 px-6 bg-green-dark text-white text-4xl text-center font-['mplus'] capitalize ">{categoria.nome}</header>
+      <header className="py-2 px-6 bg-green-600  text-4xl text-center font-['mplus'] capitalize ">{categoria.nome}</header>
       <p className='p-8 text-3xl  h-full '>{categoria.descricao}</p>
       <div className="flex">
         <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-green-600 hover:bg-green-700 flex items-center justify-center py-2'>
