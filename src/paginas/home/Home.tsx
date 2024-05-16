aa30675871f27429a4455551ef3cf0cbc0c2da69
Kamilla Lima <kamillalima868@gmail.com>
2024-05-16 17:12:05 -0300

    ajustando home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './Home.css';
-import homeBanner from '../../assets/img/banner-naturalar.png'
+import fundo from '../../assets/img/logo-green.png'
+import { useNavigate } from 'react-router-dom';
+
 
 function Home() {
-    return (
-      <>
-        <div className="bg-indigo-900 flex justify-center teste">
-        <img src={homeBanner} alt="Logo da Naturalar" className='w-3/3 imagem' />
-        </div>
-      </>
-    );
+
+  const navigate = useNavigate();
+  function produtos(){
+    navigate('/produtos')
+  }
+
+
+
+  return (
+    <>
+      <div id="primeiraParte">
+        <img src={fundo} width={110} className='mb-4' ></img>
+        <h1 id="titulo">Construa o futuro, preserve o presente: </h1>
+        <h1 id="titulo">Naturalar, sustentabilidade em cada projeto.</h1>
+        <br></br>
+        <p>Somos uma empresa nova no mercado, mas que sempre pensou em mudar o mundo a começar por nossas próprias casas. Por conta disso, desenvolvemos um local onde outras pessoas que querem fazer a sua parte para salvar o mundo possam dar o seu primeiro passo.</p>
+       <button onClick={produtos}>Nossos produtos</button>
+      </div>
+    </>
+  );
 }
 export default Home
