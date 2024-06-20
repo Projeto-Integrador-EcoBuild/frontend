17b50f20f376e1a56e4d3e1ae498528a1d3cbee6
biel678 <bieltomarchio@hotmail.com>
2024-06-19 21:58:28 -0300

    Alterações do crud de funcionarios

--- src/components/Categoria/FormCategoria/FormCategoria.tsx
+++ src/components/Categoria/FormCategoria/FormCategoria.tsx
@@ -106,20 +106,20 @@ function FormCategoria() {
 
   return (
     <div className="container flex flex-col items-center justify-center mx-auto">
-      <h1 className="text-5xl text-center my-8 ">
+      <h1 className="text-4xl text-center my-8 cp:text-3xl cp:mb-4">
         {id === undefined ? 'Cadastre uma nova Categoria' : 'Editar Categoria'}
       </h1>
 
-      <form className="w-1/2 flex flex-col gap-10" onSubmit={gerarNovoCategoria}>
+      <form className="w-1/2 flex flex-col gap-10 cp:gap-4 mb-4" onSubmit={gerarNovoCategoria}>
         <div className=" flex flex-col static">
-          <label htmlFor="nome" className="text-green-dark text-s font-semibold relative top-3 ml-[7px] px-[3px] bg-white w-fit">Nome</label>
+          <label htmlFor="nome" className="text-green-dark text-s cp:-ml-8 font-semibold relative top-3 ml-[7px] px-[3px] bg-white w-fit">Nome</label>
           <input
             type="text"
             required
             name="nome"
             minLength={5}
             maxLength={50}
-            className="border-green-dark  px-[10px] py-[11px] text-s  border-2 rounded-[5px] w-full focus:ring-0 focus:border-2 focus:border-green-hover "
+            className="border-green-dark cp:w-60 cp:-ml-10 px-[10px] py-[11px] text-s  border-2 rounded-[5px] w-full focus:ring-0 focus:border-2 focus:border-green-hover "
             value={categoria.nome}
             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
@@ -128,12 +128,12 @@ function FormCategoria() {
 
 
         <div className="input flex flex-col static">
-          <label htmlFor="nome" className='text-green-dark text-s font-semibold relative top-3 ml-[7px] px-[3px] bg-white w-fit '>Descrição</label>
+          <label htmlFor="nome" className='text-green-dark cp:-ml-8 text-s font-semibold relative top-3 ml-[7px] px-[3px] bg-white w-fit '>Descrição</label>
           <textarea
             required
             rows={4}
             name="descricao"
-            className="border-green-dark  px-[10px] py-[11px] text-s  border-2 rounded-[5px] w-full focus:ring-0 focus:border-2 focus:border-green-hover "
+            className="border-green-dark cp:w-60 cp:-ml-10 px-[10px] py-[11px] text-s  border-2 rounded-[5px] w-full focus:ring-0 focus:border-2 focus:border-green-hover "
             value={categoria.descricao}
             onChange={(e: ChangeEvent<HTMLTextAreaElement>) => atualizarEstadoTextArea(e)} />
         </div>
@@ -142,13 +142,13 @@ function FormCategoria() {
 
         <div className='flex '>
           <button
-            className="rounded text-white bg-red-400 hover:bg-red-700 w-1/3 py-4 mx-auto block text-xl"
+            className="rounded text-white cp:w-full cp:-ml-4 bg-red-400 hover:bg-red-700 w-1/3 py-4 mx-auto block text-xl"
             type="submit" onClick={retornar}
           >
             Cancelar
           </button><button
             className={`rounded text-white ${!categoria.nome ? 'disabled:bg-slate-200 ' : 'bg-green-600 hover:bg-green-700'} 
-              w-1/3 py-4 mx-auto block text-xl relative`}
+              w-1/3 cp:w-3/4 cp:ml-2 py-4 mx-auto block text-xl relative`}
             type="submit"
             disabled={!categoria.nome}
           >
@@ -165,9 +165,6 @@ function FormCategoria() {
             )}
             {!isLoading && (id === undefined ? (categoria.nome ? 'Cadastrar' : 'Carregando') : (categoria.nome ? 'Editar' : 'Carregando'))}
           </button>
-
-
-
 
         </div>
 
