ddd72877a94c4f0ac2f94721629ea6ea88026338
Kamilla Lima <kamillalima868@gmail.com>
2024-07-02 07:38:19 -0300

    alteracao card funcionario

--- src/components/Categoria/cardCategoria/cardCategoria.tsx
+++ src/components/Categoria/cardCategoria/cardCategoria.tsx
@@ -9,10 +9,10 @@ interface CardCategoriaProps {
 function CardCategoria({categoria}: CardCategoriaProps) {
   return (
     <div className='border flex flex-col rounded-1xl  justify-between border-gray-400 dark:text-white dark:border-green-dark'>
-      <header className="py-2 px-6 bg-green-claro dark:bg-green-dark h-36 flex justify-center items-center text-3xl text-center capitalize ">{categoria.nome}</header>
+      <header className="py-2 px-6 bg-green-light dark:bg-green-dark h-36 flex justify-center items-center text-3xl text-center capitalize ">{categoria.nome}</header>
       <p className='p-6 text-justify text-2xl '>{categoria.descricao}</p>
       <div className="flex">
-        <Link to={`/editarCategoria/${categoria.id}`} className=' bg-green-claro text-center p-3 w-full text-2xl uppercase 
+        <Link to={`/editarCategoria/${categoria.id}`} className=' bg-green-light text-center p-3 w-full text-2xl uppercase 
          dark:text-white dark:bg-green-botao dark:hover:bg-green-hover
          text-green-hover hover:bg-green-hover hover:text-white '>
           <button>Editar</button>
