76b127e451e965322d67ce6791e6bcc1fe3fc223
Kamilla Lima <kamillalima868@gmail.com>
2024-07-02 06:02:46 -0300

    problema na rota da pagina do item

--- src/components/produtos/itemProduto/ItemProduto.tsx
+++ src/components/produtos/itemProduto/ItemProduto.tsx
@@ -7,28 +7,21 @@ import Categoria from '../../../models/Categoria';
 import { buscar } from '../../../services/Service';
 import { toastAlerta } from '../../../util/toastAlerta'
 import './itemProduto.css'
-
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-
-// import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 import ModalDetalhesParcelamento from '../modalProduto/modalDetalhesParcelamento';
 import CardProduto from '../cardProduto/CardProduto';
-import { House } from '@phosphor-icons/react'
 
 function ItemProduto() {
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
   const { usuario } = useContext(AuthContext);
   const token = usuario.token;
-  const { adicionarProduto } = useContext(AuthContext)
+  const { adicionarProduto } = useContext(AuthContext);
+
   const [categoria, setCategoria] = useState<Categoria>({
     id: 0,
     nome: '',
@@ -46,8 +39,9 @@ function ItemProduto() {
     quantidadeComprada: 0
   });
 
-  const [listaOpcoes, setlistaOpcoes] = useState<Produto[]>([]);
+  const [nomeCategoria, setNomeCategoria] = useState('');
 
+  const [listaOpcoes, setlistaOpcoes] = useState<Produto[]>([]);
 
   async function buscarProdutoPorCategoria(id: string) {
     await buscar(`/produtos/categoria/${id}`, setlistaOpcoes, {
@@ -56,7 +50,6 @@ function ItemProduto() {
       },
     });
   }
-
 
   async function buscarProdutoPorId(id: string) {
     await buscar(`/produtos/${id}`, setProduto, {
@@ -72,37 +65,34 @@ function ItemProduto() {
 
   useEffect(() => {
     if (produto.categoria?.id) {
-      const idCategoriaBusca = produto.categoria.id.toString()
+      const idCategoriaBusca = produto.categoria.id.toString();
       buscarProdutoPorCategoria(idCategoriaBusca);
+      setNomeCategoria(produto.categoria.nome || '');
     }
-  }, [produto.categoria?.id]);
+  }, [produto.categoria?.id, produto.categoria?.nome]);
+
   function verificarUsuario(product: Produto) {
     if (token === "") {
-      toastAlerta('Você deve realizar o login! ', 'erro')
-      navigate('/login')
-    }
-    else {
+      toastAlerta('Você deve realizar o login! ', 'erro');
+      navigate('/login');
+    } else {
       if (verificarProdutosEmEstoque(produto.quantidadeComprada, produto.quantidade)) {
         alertaQuantidadeSemEstoque();
         return;
       }
       verificarSeQtdCompradaEstaIndefinido();
-      adicionarProduto(product)
-      toastAlerta('Produto adicionado ao carrinho', 'info')
+      adicionarProduto(product);
+      toastAlerta('Produto adicionado ao carrinho', 'info');
     }
   }
 
   function continuarCompra() {
-    navigate(-1)
-  }
-
-  function visitarProdutosCategoria() {
-    navigate(`/produtos/categoria/${produto.categoria?.id}`)
+    navigate('/produtos');
   }
 
   function verificarSeQtdCompradaEstaIndefinido() {
     if (produto.quantidadeComprada === undefined) {
-      produto.quantidadeComprada = 1
+      produto.quantidadeComprada = 1;
     }
   }
 
@@ -117,22 +107,21 @@ function ItemProduto() {
 
   function verificarProdutosEmEstoque(quantidadeComprada: number, quantidadeEmEstoque: number) {
     return quantidadeComprada > quantidadeEmEstoque;
-
   }
-
 
   function alertaQuantidadeSemEstoque() {
-    alert("não possuimos essa quantidade de itens para venda")
+    alert("não possuimos essa quantidade de itens para venda");
   }
-
-
+  function visitarProdutosCategoria() {
+    navigate(`/produtos/categoria/${produto.categoria?.id}`)
+  }
 
   return (
     <div className='dark:text-white'>
-      <p className=' text-center py-10 font-medium cp:text-xs '> <a ><button onClick={continuarCompra} > Página inicial </button></a> <button onClick={visitarProdutosCategoria}> &gt; {produto.categoria?.nome}</button> &gt; <span className='underline capitalize'> {produto.nome}</span></p>
-      <div className='flex items-center gap-8 justify-center cp:flex-col  sm:flex-col 2xl:gap-6 2xl:mx-20  '>
-        <div className=' w-[28rem] h-80 cp:w-[80%] sm:w-[90%] 2xl:w-[35%] -mt-11 cp:mt-0 sm:mt-0 md:w-[40%] '>
-
+      <p className=' text-center py-10 font-medium cp:text-xs '>
+      <p className=' text-center py-10 font-medium cp:text-xs '> <a ><button onClick={continuarCompra} > Página inicial </button></a> <button onClick={visitarProdutosCategoria}> &gt; {nomeCategoria}</button> &gt; <span className='underline capitalize'> {produto.nome}</span></p>      </p>
+      <div className='flex items-center gap-8 justify-center cp:flex-col sm:flex-col 2xl:gap-6 2xl:mx-20'>
+        <div className=' w-[28rem] h-80 cp:w-[80%] sm:w-[90%] 2xl:w-[35%] -mt-11 cp:mt-0 sm:mt-0 md:w-[40%]'>
           <Swiper
             slidesPerView={1}
             loop={true}
@@ -143,51 +132,47 @@ function ItemProduto() {
             modules={[Pagination, Navigation]}
             className="mySwiper"
           >
-            <SwiperSlide>          <img src={produto.foto} className='object-cover' alt="imagem do produto" />
+            <SwiperSlide>
+              <img src={produto.foto} className='object-cover' alt="imagem do produto" />
             </SwiperSlide>
-            <SwiperSlide>          <img src={produto.foto} className='object-cover' alt="imagem do produto" />
+            <SwiperSlide>
+              <img src={produto.foto} className='object-cover' alt="imagem do produto" />
             </SwiperSlide>
-            <SwiperSlide>          <img src={produto.foto} className='object-cover' alt="imagem do produto" />
+            <SwiperSlide>
+              <img src={produto.foto} className='object-cover' alt="imagem do produto" />
             </SwiperSlide>
-            <SwiperSlide>          <img src={produto.foto} className='object-cover' alt="imagem do produto" />
+            <SwiperSlide>
+              <img src={produto.foto} className='object-cover' alt="imagem do produto" />
             </SwiperSlide>
-
           </Swiper>
         </div>
-        <div className=" space-y-4 w-1/2 cp:w-[80%] sm:w-[80%]  ">
+        <div className=" space-y-4 w-1/2 cp:w-[80%] sm:w-[80%]">
           <h1 className='text-4xl capitalize font-semibold cp:text-3xl cp:text-center md:text-3xl text-amber-950 dark:text-white'>{produto.nome}</h1>
           <p className='text-gray-400'>Cód: {produto.id}</p>
-          <p className='text-justify text-xl '>
-            {produto.descricao}       </p>
-          <p className=''>Por R${produto.preco.toFixed(2).replace(".", ",")} ou <span className='font-bold text-green-hover text-2xl'>R${(produto.preco - (produto.preco * 0.10)).toFixed(2).replace(".", ",")}</span> no PIX </p>
-
+          <p className='text-justify text-xl'>
+            {produto.descricao}
+          </p>
+          <p>Por R${produto.preco.toFixed(2).replace(".", ",")} ou <span className='font-bold text-green-hover text-2xl'>R${(produto.preco - (produto.preco * 0.10)).toFixed(2).replace(".", ",")}</span> no PIX</p>
           {produto.preco < 10 ? <></> : <p>
             <span className='font-bold'>2X </span>de <span className='font-bold'>R${(produto.preco / 2).toFixed(2).replace(".", ",")} </span>sem juros
-          </p>
-          }
-
+          </p>}
           <ModalDetalhesParcelamento preco={produto.preco} />
-          <div className='flex items-center  w-full justify-around   '>
-            <label htmlFor="quantidadeComprada" ></label>
+          <div className='flex items-center w-full justify-around'>
+            <label htmlFor="quantidadeComprada"></label>
             <input type="number"
               onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
               value={produto.quantidadeComprada}
-              className=' border-green-light px-5 text-xl uppercase py-3 rounded-lg cp:text-base cp:px-2 md:text-base dark:text-black'
+              className='border-green-light px-5 text-xl uppercase py-3 rounded-lg cp:text-base cp:px-2 md:text-base dark:text-black'
               max={produto.quantidade}
               name="quantidadeComprada"
               min={1} />
-            <button className="bg-green-light  font-bold text-black px-5 text-xl uppercase py-3 rounded-lg
-             hover:bg-green-hover hover:text-white  dark:bg-green-hover dark:text-white dark:hover:text-black dark:hover:bg-green-light cp:px-2 cp:text-base  md:text-base" onClick={() => verificarUsuario(produto)}>Adicionar ao Carrinho</button>
+            <button className="bg-green-light font-bold text-black px-5 text-xl uppercase py-3 rounded-lg
+             hover:bg-green-hover hover:text-white dark:bg-green-hover dark:text-white dark:hover:text-black dark:hover:bg-green-light cp:px-2 cp:text-base md:text-base" onClick={() => verificarUsuario(produto)}>Adicionar ao Carrinho</button>
           </div>
         </div>
-
       </div>
-
-      <div className='mt-16 pb-16 px-12 w-full '>
+      <div className='mt-16 pb-16 px-12 w-full'>
         <p className='font-normal text-3xl text-amber-950 dark:text-white'>Produtos relacionados</p>
-
-
-
         {listaOpcoes.length !== 1 ? (
           <div className='grid grid-cols-4 cp:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 dark:border-t-white border-t-gray-400 border-t pt-12 mt-2'>
             {listaOpcoes
@@ -203,12 +188,7 @@ function ItemProduto() {
           </p>
         )}
       </div>
-
-
-
-
     </div>
-
   );
 }
 
